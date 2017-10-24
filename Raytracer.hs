6a03ad5ed7609d1eedc0b54893737d4ca35f760a
Denis M <Nolane16@gmail.com>
2017-10-25 00:59:02 +0300

    Add triangles as scene objects

--- Raytracer.hs
+++ Raytracer.hs
@@ -2,6 +2,8 @@ module Raytracer(tracePixel) where
 
 import Control.Applicative
 import Data.List
+import Data.Maybe
+import Data.Ord
 import Debug.Trace
 
 import Color
@@ -10,23 +12,26 @@ import Vector
 type Position = Vector
 type Direction = Vector
 
-data Ray =
-    Ray
+data Ray
+    = Ray
     { raySource :: Position
     , rayDirection :: Direction
     }
 
-data Sphere =
-    Sphere
+data Sphere
+    = Sphere
     { sphereCenter :: Position
     , sphereRadius :: Double
     , sphereColor :: Color
     }
 
+data Triangle = Triangle Position Position Position Color
+
 class SceneObject a where
     {- Casts a ray. If it intersects object returns position of first
     intersection and normal of object in that point. -}
     cast :: Ray -> a -> Maybe (Position, Direction)
+    objectColor :: a -> Color
 
 solveQuadraticEquation :: Double -> Double -> Double -> [Double]
 solveQuadraticEquation a b c
@@ -37,58 +42,110 @@ solveQuadraticEquation a b c
     d = b * b - 4 * a * c
 
 instance SceneObject Sphere where
-    cast ray sphere
+    cast (Ray x0 d) (Sphere xc r _)
         | null positiveTs = Nothing
         | otherwise       = Just $ xn $ head positiveTs
       where
-        positiveTs = sort $ filter (0 <) t
-        x  t = x0 + (d `vscale` t)
+        positiveTs = sort $ filter (0 <) ts
+        x  t = x0 + (d * pure t)
         n  t = vnormal $ x t - xc
         xn t = (x t, n t)
-        t = solveQuadraticEquation a b c
+        ts = solveQuadraticEquation a b c
         a = d `vdot` d
         b = 2 * ((x0 - xc) `vdot` d)
         c = (x0 - xc) `vdot` (x0 - xc) - r^2
-        x0 = raySource ray
-        d  = rayDirection ray
-        xc = sphereCenter sphere
-        r  = sphereRadius sphere
+    objectColor = sphereColor
+
+instance SceneObject Triangle where
+    cast (Ray x0 d) (Triangle a b c _)
+        | 0 < t
+        , 0 <= k, 0 <= l
+        , k + l <= 1
+        , 0 /= d `vdot` n
+            = Just (p, n)
+        | otherwise
+            = Nothing
+      where
+        n = vnormal $ (b - a) `vcross` (c - a)
+        t = -((x0 - a) `vdot` n) / (d `vdot` n)
+        p = x0 + pure t * d
+        Vec k l _ = toBasis (b - a, c - a, n) (p - a)
+    objectColor (Triangle _ _ _ c) = c
+
+data Object
+    = SphereObject Sphere
+    | TriangleObject Triangle
+
+instance SceneObject Object where
+    cast ray (SphereObject s)   = cast ray s
+    cast ray (TriangleObject t) = cast ray t
+    objectColor (SphereObject s) = objectColor s
+    objectColor (TriangleObject t) = objectColor t
 
 data Camera =
     Camera
-    { camPosition :: Position
-    , camTopLeft :: Position
-    , camTopRight :: Position
+    { camPosition   :: Position
+    , camTopLeft    :: Position
+    , camTopRight   :: Position
     , camBottomLeft :: Position
     }
 
 getRay :: Camera -> Double -> Double -> Ray
 getRay c x y = Ray (camPosition c) direction
   where
-    dx = (camTopRight c   - camTopLeft c) `vscale` x
+    dx = (camTopRight c - camTopLeft c) `vscale` x
     dy = (camBottomLeft c - camTopLeft c) `vscale` y
     direction = vnormal $ (camTopLeft c + dx + dy) - camPosition c
 
 data LightSource = LightSource Position Color
 
 camera = Camera
-    { camPosition   = Vec 0 0 0 0
-    , camTopLeft    = Vec (-0.5) 0.5 (-0.5) 0
-    , camTopRight   = Vec 0.5 0.5 (-0.5) 0
-    , camBottomLeft = Vec (-0.5) (-0.5) (-0.5) 0
+    { camPosition   = Vec 0 0 0
+    , camTopLeft    = Vec (-0.5) 0.5 (-0.5)
+    , camTopRight   = Vec 0.5 0.5 (-0.5)
+    , camBottomLeft = Vec (-0.5) (-0.5) (-0.5)
     }
 
-sphere =
-    Sphere
-    { sphereCenter = Vec 0 0 (-7) 0
-    , sphereRadius = 4
-    , sphereColor  = Color 1 1 1
-    }
+sphere :: Vector -> Double -> Color -> Object
+sphere p r c = SphereObject $ Sphere p r c
+
+triangle :: Vector -> Vector -> Vector -> Color -> Object
+triangle a b c color = TriangleObject $ Triangle a b c color
+
+plane :: Vector -> Vector -> Vector -> Vector -> Color -> [Object]
+plane a b c d color =
+    [ triangle a c b color
+    , triangle a d c color
+    ]
+
+objects =
+    plane a b c d red ++
+    plane e f b a green ++
+    plane g h d c blue ++
+    plane d h e a purple ++
+    plane b f g c purple ++
+    [ sphere (Vec (-6) (-6) (-16)) 4 white
+    , sphere (Vec 0 (-6) (-12)) 2 white
+    , sphere (Vec (-3) (-7) (-10)) 1 white
+    ]
+  where
+    a = Vec 10 10 (-20)
+    b = Vec 10 (-10) (-20)
+    c = Vec (-10) (-10) (-20)
+    d = Vec (-10) 10 (-20)
+    [e, f, g, h] = map (\(Vec x y z) -> Vec x y (-z)) [a, b, c, d]
+    red    = Color 0.8 0.4 0.4
+    green  = Color 0.4 0.8 0.4
+    blue   = Color 0.4 0.4 0.8
+    purple = Color 0.4 0.8 0.8
+    white  = Color 0.8 0.8 0.8
 
 lightSources =
-    [ LightSource (Vec (10) (10) 0 0)   (Color 1 0 0)
-    , LightSource (Vec (-10) (-10) 0 0) (Color 0 1 0)
-    , LightSource (Vec 5 0 (-2) 0)      (Color 0 0 1)
+    [ LightSource (Vec 0 8 (-10)) (Color 0.8 0.8 1)
+    , LightSource (Vec (-9) (-9) (-13)) (Color 1 0.5 0)
+    , LightSource (Vec (-2 + 3) (-2) (-12)) (Color 1 0 0)
+    , LightSource (Vec (-2) (-2 + 3) (-12)) (Color 0 1 0)
+    , LightSource (Vec (-2) (-2) (-12 + 3)) (Color 0 0 1)
     ]
 
 {- https://en.wikipedia.org/wiki/Phong_reflection_model -}
@@ -101,18 +158,23 @@ phongIllumination material fragmentPosition n v =
         , let hm = vnormal $ lm + v
         ]
   where
-    ks = material * Color 0.7 0.7 0.7
-    kd = material * Color 0.7 0.7 0.7
-    ka = material * Color 0.2 0.2 0.2
-    alpha = 1000 :: Double
+    ks = material * Color 0.5 0.5 0.5
+    kd = material * Color 0.4 0.4 0.4
+    ka = material * Color 0.3 0.3 0.3
+    alpha = 300 :: Double
     ambient = Color 1 1 1
+
+distanceToCamera :: Camera -> Vector -> Double
+distanceToCamera c p = vlen (p - camPosition c)
 
 tracePixel :: Double -> Double -> (Int, Int, Int)
 tracePixel x y
-    | Nothing <- c = black
-    | Just (p, n) <- c
-        = toGuiColor $ phongIllumination (sphereColor sphere) p n (-direction)
+    = case [ (s, p, n) | s <- objects, Just (p, n) <- [ cast ray s ] ] of
+        []   -> (0, 0, 0)
+        list ->
+            let z = comparing (\(_, p, _) -> distanceToCamera camera p)
+                (s, p, n) = minimumBy z list
+            in toGuiColor $ phongIllumination (objectColor s) p n (-direction)
   where
     ray@(Ray _ direction) = getRay camera x y
-    c = cast ray sphere
     black = (0, 0, 0)
--- Vector.hs
+++ Vector.hs
@@ -2,33 +2,32 @@ module Vector where
 
 import Control.Applicative
 
-data GenericVector a = Vec a a a a
+data GenericVector a = Vec a a a
     deriving Eq
 
 type Vector = GenericVector Double
 
 instance Show a => Show (GenericVector a) where
-    show (Vec x y z w) =
-        "(" ++ x' ++ ", " ++ y' ++ ", " ++ z' ++ ", " ++ w' ++ ")"
+    show (Vec x y z) =
+        "(" ++ x' ++ ", " ++ y' ++ ", " ++ z' ++ ")"
       where
         x' = show x
         y' = show y
         z' = show z
-        w' = show w
 
 instance Functor GenericVector where
-    fmap f (Vec x y z w) = Vec (f x) (f y) (f z) (f w)
+    fmap f (Vec x y z) = Vec (f x) (f y) (f z)
 
 instance Applicative GenericVector where
-    pure x = (Vec x x x x)
-    (Vec f g h i) <*> (Vec x y z w) = Vec (f x) (g y) (h z) (i w)
+    pure x = (Vec x x x)
+    (Vec f g h) <*> (Vec x y z) = Vec (f x) (g y) (h z)
 
 instance Num a => Num (GenericVector a) where
     (+) = liftA2 (+)
     (*) = liftA2 (*)
     abs x = abs <$> x
     signum x = signum <$> x
-    fromInteger x = fromInteger <$> Vec x x x x
+    fromInteger x = fromInteger <$> Vec x x x
     negate x = negate <$> x
 
 vscale :: Vector -> Double -> Vector
@@ -37,7 +36,7 @@ vscale u f = (f *) <$> u
 vdot :: Vector -> Vector -> Double
 vdot = (sum .) . liftA2 (*)
   where
-    sum (Vec a b c d) = a + b + c + d
+    sum (Vec a b c) = a + b + c
 
 vlen :: Vector -> Double
 vlen v = sqrt $ v `vdot` v
@@ -47,3 +46,22 @@ vcos u v = (u `vdot` v) / (vlen u * vlen v)
 
 vnormal :: Vector -> Vector
 vnormal v = v `vscale` (1 / vlen v)
+
+type Basis = (Vector, Vector, Vector)
+
+determenant :: Basis -> Double
+determenant (Vec a b c, Vec d e f, Vec g h i)
+    = a * e * i + b * f * g + c * d * h - a * f * h - b * d * i - c * e * g
+
+toBasis :: Basis -> Vector -> Vector
+toBasis basis@(v1@(Vec a b c), v2@(Vec d e f), v3@(Vec g h i)) v =
+    Vec (d1 / d) (d2 / d) (d3 / d)
+  where
+    d = determenant basis
+    d1 = determenant (v, v2, v3)
+    d2 = determenant (v1, v, v3)
+    d3 = determenant (v1, v2, v)
+
+vcross :: Vector -> Vector -> Vector
+(Vec x y z) `vcross` (Vec x' y' z') =
+    Vec (y * z' - z * y') (z * x' - x * z') (x * y' - y * x')
